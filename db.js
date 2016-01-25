9c98b496520304581214daccbf02b9ab82273a7b
=Haider Malik <haidermalik5044@gmail.com>
2016-01-25 13:11:28 -0800

    Add Expaned Users in to the documents not use Alter here

---
+++ db.js
@@ -0,0 +1,53 @@
+var mongodb=require('mongodb');
+
+//we need to work with MongoClient interface in order to connect to a mongodb server
+var MongoClient=mongodb.MongoClient;
+
+//Connection URL
+var url="mongodb://localhost:27017/users";
+
+MongoClient.connect(url,function(err,db){
+
+	if(err){
+		console.log('Unable to connect to the Mongodb Server Error'+err);	
+	}
+	else{
+		console.log('Connection established to '+url);
+
+        var users=db.collection('users');
+
+         //without expanded users
+        var haiderMalik1=({
+        	name:'Haider Malik',
+        	city:'FSD'
+        });
+
+        //Afrter expanded Users
+        var haiderMalik_exp=({
+        	name:'Haider Malik',
+        	city:'FSD',
+        	favt_song:'Like Home'
+        });
+
+        //After Expandeed Users
+        var haiderMalik_exp1=({
+          name:'Haider Malik',
+          city:'FSD',
+          favt_song:'illuminate',
+          favt_color:'White,Blue,Black'
+        });
+
+         users.insert([haiderMalik1,haiderMalik_exp,haiderMalik_exp1],function(err,result){
+         	if(err){
+         		console.log('Unable to save the users '+err);
+         	}
+         	else{
+         		console.log('user saved to document: '+result);
+         	}
+         });
+
+
+
+		//db.close();
+	}
+});
